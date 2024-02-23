fae48234ba2fc30622cf603abb4ddee360c6bc8f
Luis Azevedo <47978094+luisazvd@users.noreply.github.com>
2024-02-22 22:33:52 -0300

    Add files via upload

---
+++ aula.dart
@@ -0,0 +1,70 @@
+void main(List<String> arguments) {
+  // print('Olá, mundo');
+
+  // String nomeAluno = 'Luis';
+  // String sobrenomeAluno = 'Azevedo';
+
+  // int idadeAluno = 21;
+
+  // double peso = 100.0;
+
+  // bool eAprovado = true;
+
+  // print(nomeAluno + ' ' + sobrenomeAluno);
+  // print('${nomeAluno} ${sobrenomeAluno}');
+  // print('$nomeAluno $sobrenomeAluno');
+  // print('$nomeAluno ${idadeAluno >= 18 ? 'não precisa' : 'precisa'}');
+
+//ATIVIDADE 01
+  // String disciplina = 'DDM';
+  // double cargaHoraria = 80.5;
+  // String professor = 'Hélio';
+  // bool eAtivo = true;
+  // int quantidadeAluno = 25;
+
+  // print(disciplina + ' ' + professor);
+  // print('$disciplina $quantidadeAluno');
+  // print('$cargaHoraria ${quantidadeAluno >= 25 ? 'Presentes' : 'não presentes'}');
+
+//ATIVIDADE 02
+
+  // String nome = 'Dr. Jorge';
+
+  // String funcao = 'Médico';
+
+  // int idade = 35;
+
+  // bool eAtivo = true;
+
+  // print(nome +' '+ funcao);
+
+  // print('$funcao ${eAtivo == true ? 'Atende' : 'Não atende'}');
+
+  // print('variáveis de inferência');
+  // var nome = 'João';
+  // var sobrenome;
+  // sobrenome = 'Silva';
+
+  // print('variáveis dinâmicas'); //não é boa prática
+  // dynamic nomeAluno = 'Maria';
+  // nomeAluno = 10;
+
+  // print('constante e final'); //otimiza o código, pois o compilador não pergunta sempre qual o valor da mesma
+  // const int codigo = 55; //em tempo de compilação
+  // final int codigoJogo = 100; //em tempo de execução
+
+  //   Atividade 03
+  //  * a) Refaça "Atividade 01" utilizando Inferência de Tipo
+  // var disciplina = 'Construção de Software';
+  // var cargaHoraria = 40.5;
+  // var professor = 'Marcelo';
+  // var eAtivo = true;
+  // var quantidadeAluno = 25;
+
+  //  Atividade 04
+  //  * Aplique o que aprendeu no seu projeto (dynamic)
+
+  dynamic registroPaciente = 654;
+  registroPaciente = 'A100';
+
+}
